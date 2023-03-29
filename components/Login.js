abc041361fb64548da4ac74f2a810904e1aa212b
FranciscoDelira <frankk230700@gmail.com>
2023-03-28 21:19:47 -0600

    Login update

--- components/Login.js
+++ components/Login.js
@@ -1,24 +1,25 @@
 import React from "react";
-import { Text, Box, Image, FormControl, Input, Link, Button, useColorMode, VStack, Center } from "native-base";
+import { Text, Image, FormControl, Input, Button, VStack } from "native-base";
 
 function Login({ navigation }) {
 
   return (
     <VStack width="100%" height="100%" _dark={{ bg: "tema.3" }} _light={{ bg: "tema.2" }} alignItems="center">
 
-      <Image width="61.2%" height="32%"  marginTop="18%" source={require('../assets/TNM1B.png')} alt="TecLogo" />
+      <Image width="61.2%" height="32%" marginTop="18%" source={require('../assets/TNM1B.png')} _dark={{ color: "tema.2", tintColor: "tema.2" }} _light={{ color: "tema.3", tintColor: "tema.3" }} alt="TecLogo" />
 
       <FormControl width="70%" height="18%" marginTop="10%">
-        <Input color={"tema.3"} size={"md"} height="50%" _focus={{ backgroundColor: "tema.2" }} borderTopLeftRadius={20} borderTopRightRadius={20} borderColor={"tema.1"} bg={"tema.2"} placeholder="Correo Institucional" />
-        <Input color={"tema.3"} size={"md"} height="50%" _focus={{ backgroundColor: "tema.2" }} borderRadius={0} borderBottomLeftRadius={20} borderBottomRightRadius={20} borderColor={"tema.1"} bg={"tema.2"} placeholder="Contraseña" />
+        <Input _dark={{ bg: "tema.2", color: "tema.3", placeholderTextColor: "tema.3" }} _light={{ bg: "tema.3", color: "tema.2", placeholderTextColor: "tema.2" }} borderColor="tema.1" fontSize="xl" height="50%" _focus={{ bg: "tema.6", borderColor: "tema.1" }} borderRadius={0} borderTopLeftRadius={20} borderTopRightRadius={20} placeholder="Correo Institucional" />
+        <Input _dark={{ bg: "tema.2", color: "tema.3", placeholderTextColor: "tema.3" }} _light={{ bg: "tema.3", color: "tema.2", placeholderTextColor: "tema.2" }} borderColor="tema.1" fontSize="xl" height="50%" _focus={{ bg: "tema.6", borderColor: "tema.1" }} borderRadius={0} borderBottomLeftRadius={20} borderBottomRightRadius={20} placeholder="Contraseña" />
       </FormControl>
 
-      <VStack width="100%" height="25%" alignItems="center" > 
-        <Button width="150px" borderRadius={"15"}  marginTop="15%" _dark={{ bg: "tema.2" }} _light={{ bg: "tema.3" }} onPress={() => navigation.navigate("maintenance")}>
-          <Text _dark={{ color: "tema.3" }} _light={{ color: "tema.2" }} >Iniciar Sesión</Text>
+      <VStack width="100%" height="25%" alignItems="center" >
+
+        <Button width="60%" borderRadius={"10"} marginTop="8%" _pressed={{ bg: 'tema.6' }} _dark={{ bg: "tema.2" }} _light={{ bg: "tema.3" }} onPress={() => navigation.navigate("maintenance")}>
+          <Text fontSize="xl" _dark={{ color: "tema.3" }} _light={{ color: "tema.2" }} >Iniciar Sesión</Text>
         </Button>
 
-        <Text  alignSelf="flex-end" marginTop="5%" marginRight="3%" fontSize="xs" underline opacity="50" _dark={{ color: "tema.2" }} _light={{ color: "tema.3" }}>¿Has Olvidado Tu Contraseña?</Text>
+        <Text alignSelf="flex-end" marginTop="8%" marginRight="3%" fontSize="xs" underline opacity="50" _dark={{ color: "tema.2" }} _light={{ color: "tema.3" }}>¿Has Olvidado Tu Contraseña?</Text>
 
         <Text alignSelf="center" marginTop="30%" opacity="20" _dark={{ color: "tema.2" }} _light={{ color: "tema.3" }}>FROM  ITA</Text>
 
