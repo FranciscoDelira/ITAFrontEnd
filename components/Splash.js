a69af658972a245b759c5e564ee316c9bea41bea
FranciscoDelira <frankk230700@gmail.com>
2023-03-28 20:54:30 -0600

    Update theme

--- components/Splash.js
+++ components/Splash.js
@@ -1,17 +1,20 @@
 import React from "react";
 import { Text, Box, Image } from "native-base";
 
+function Splash() {
 
-
-function Splash () {
-  
   return (
-    <Box style={{width:"100%", height:"100%"}} bg={"tema.3"} justifyContent='center' alignItems="center">
-       <Image alignSelf={"center"} justifyContent='center' size="150px" source={require('../assets/TNM1B.png')} alt="TecLogo"/>
-       <Box position={"absolute"} bottom={0}>
-       <Text color={"tema.2"}>FROM</Text>
-       <Text color={"tema.2"}>ITIC'S</Text>
+    <Box style={{ width: "100%", height: "100%" }} _dark={{ bg: "tema.3" }} _light={{ bg: "tema.2" }} bg={"tema.3"} justifyContent='center' alignItems="center">
+      
+      <Image alignSelf={"center"} justifyContent='center' size="150px" source={require('../assets/TNM1B.png')} _dark={{ color: "tema.2", tintColor: "tema.2" }} _light={{ color: "tema.3", tintColor: "tema.3" }} alt="TecLogo" />
+
+      <Box position={"absolute"} bottom={0}>
+        
+        <Text>FROM</Text>
+        <Text>ITIC'S</Text>
+
       </Box>
+
     </Box>
   );
 }
