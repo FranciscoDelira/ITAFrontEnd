f7017c5a844986dde27849cff4fd8ac6e63f6210
FranciscoDelira <frankk230700@gmail.com>
2023-05-02 10:19:24 -0600

    Nav.1

--- components/Navegacion.js
+++ components/Navegacion.js
@@ -27,22 +27,22 @@ function Navegacion () {
         <Stack.Navigator screenOptions={{headerShown: false }} >
             <Stack.Screen name="login" component={Login}/>
             <Stack.Screen name="password" component={Password}/>
-            <Stack.Screen name="" component={ForMaiReq}/>
-            <Stack.Screen name="" component={ForWorOrd}/>
-            <Stack.Screen name="" component={MenMaiAct}/>
-            <Stack.Screen name="" component={MenMaiHis}/>
-            <Stack.Screen name="" component={MenMaiReq}/>
-            <Stack.Screen name="" component={MenWorOrd}/>
-            <Stack.Screen name="" component={MenWorOrdApr}/>
-            <Stack.Screen name="" component={MenWorOrdPen}/>
-            <Stack.Screen name="" component={MenWorOrdRel}/>
-            <Stack.Screen name="" component={Profile}/>
-            <Stack.Screen name="" component={RelOrd}/>
-            <Stack.Screen name="" component={Settings}/>
-            <Stack.Screen name="" component={VieMaiRel}/>
-            <Stack.Screen name="" component={VieMaiReq}/>
-            <Stack.Screen name="" component={VieWorOrdApr}/>
-            <Stack.Screen name="" component={VieWorOrdPen}/>
+            <Stack.Screen name="formaireq" component={ForMaiReq}/>
+            <Stack.Screen name="forworord" component={ForWorOrd}/>
+            <Stack.Screen name="menmaiact" component={MenMaiAct}/>
+            <Stack.Screen name="menmaihis" component={MenMaiHis}/>
+            <Stack.Screen name="menmainreq" component={MenMaiReq}/>
+            <Stack.Screen name="menworord" component={MenWorOrd}/>
+            <Stack.Screen name="menwordordapr" component={MenWorOrdApr}/>
+            <Stack.Screen name="menworordpen" component={MenWorOrdPen}/>
+            <Stack.Screen name="menworordrel" component={MenWorOrdRel}/>
+            <Stack.Screen name="profile" component={Profile}/>
+            <Stack.Screen name="relord" component={RelOrd}/>
+            <Stack.Screen name="settings" component={Settings}/>
+            <Stack.Screen name="viemairel" component={VieMaiRel}/>
+            <Stack.Screen name="viemaireq" component={VieMaiReq}/>
+            <Stack.Screen name="vieworordapr" component={VieWorOrdApr}/>
+            <Stack.Screen name="vieworordpen" component={VieWorOrdPen}/>
         </Stack.Navigator>
     )
 }
