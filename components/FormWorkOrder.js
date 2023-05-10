98de1d75d71ff21eef96b7bd987d6516ad41ddb5
FranciscoDelira <frankk230700@gmail.com>
2023-05-10 16:10:12 -0600

    Listo

--- components/FormWorkOrder.js
+++ components/FormWorkOrder.js
@@ -1,8 +1,10 @@
-import React from "react";
-import { VStack, Text, Heading, Image, Box, HStack, AlertDialog, Button, TextArea, Divider } from "native-base";
-import { TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { VStack, Text, Heading, Image, Box, HStack, AlertDialog, Button, TextArea, Input } from "native-base";
+import DatePicker, { getFormatedDate } from "react-native-modern-datepicker";
+import { View, Modal, TouchableOpacity } from "react-native";
 
-function ForWorOrd({navigation}) {
+function ForWorOrd(){
+    //Estados para abrir y cerrar alerts
     const [Info, setIsOpen] = React.useState(false);
     const CloseI = () => setIsOpen(false);
 
@@ -15,47 +17,115 @@ function ForWorOrd({navigation}) {
     const [Confirm, setIsOpen3] = React.useState(false);
     const CloseC = () => setIsOpen3(false);
 
+    //Estados para seleccionar fecha
+    const today = new Date();
+    const startDate = getFormatedDate(today.setDate(today.getDate() + 1), 'DD/MM/YYYY')
+
+    const [openM, setOpenM] = useState(false); //abrir y cerrar modal
+    const [date, setDate] = useState('Seleccionar'); //variable de fecha
+    const [text, setText] = useState('Seleccionar');
+
+    function handleOnPress(){
+        setOpenM(!openM);
+    }
+
+    function handleChange (propDate){
+        setDate(propDate)
+        setText(date)
+    }
+
+    //Estados para cargar imagen
+    const [galleryPhoto, setGalleryPhoto] = useState();
+
+    let options = {
+        saveToPhotos: true,
+        mediaType: 'photo',
+    }
+
+    const openGallery = async() => {
+        const result = await launchImageLibrary(options);
+        setGalleryPhoto(result.assets[0].uri)
+    }
+    
     return (
         <VStack height="100%" width="100%" space={4} >
             <Heading _dark={{ color: "tema.3" }} _light={{ color: "tema.2" }} textAlign="center" height="15%" alignSelf="center" fontSize="3xl" paddingTop="10%">Órden de trabajo de Mantenimiento</Heading>
 
             <Box height="70%" w="90%" alignSelf="center">
-                <Text _dark={{ color: "tema.3" }} _light={{ color: "tema.2" }} alignSelf="flex-end" fontSize="md">Folio: 001</Text>
-                <Text _dark={{ color: "tema.3" }} _light={{ color: "tema.2" }} fontSize="md" marginTop="3%">Fecha de mantenimiento: <Date></Date></Text>
-                <Text _dark={{ color: "tema.3" }} _light={{ color: "tema.2" }} fontSize="md" marginTop="3%">Trabajo realizado:</Text>
-                <TextArea h={40} _dark={{ color: "tema.3" }} _light={{ color: "tema.2" }}></TextArea>
-                <Text _dark={{ color: "tema.3" }} _light={{ color: "tema.2" }} fontSize="md" marginTop="3%">Añadir Evidencia (subir imagen)</Text>
-                <Text _dark={{ color: "tema.3" }} _light={{ color: "tema.2" }} fontSize="md" marginTop="3%">(Imagen1)  (Imagen2)  (Imagen3)</Text>
-                <Divider my="2" marginTop="5%" _light={{bg:"tema.2"}} _dark={{bg: "tema.3"}} />
-                <Button onPress={() => setIsOpen2(!Send)} marginTop="6%" rounded="lg" w="40%" alignSelf="center" _pressed={{ bg: 'tema.6' }} _dark={{ bg: "tema.3" }} _light={{ bg: "tema.2" }}>Enviar
-                    <AlertDialog isOpen={Send} onClose={CloseS}>
-                        <AlertDialog.Content _dark={{ bg: "tema.2" }} _light={{ bg: "tema.3" }}>
-                            <AlertDialog.Body _dark={{ bg: "tema.2" }} _light={{ bg: "tema.3" }} alignSelf="center">
-                                <TouchableOpacity onPress={CloseS}>
-                                    <Image size="5" marginLeft="90%" marginBottom="10%" source={require('../assets/XA.png')} _dark={{ color: "tema.3", tintColor: "tema.3" }} _light={{ color: "tema.2", tintColor: "tema.2" }} alt="close" />
-                                </TouchableOpacity>
-                                <Text _dark={{ color: "tema.3" }} _light={{ color: "tema.2" }} fontSize="md" textAlign="center">¿Esta seguro de enviar la Información?</Text>
-                                <Button.Group marginTop="10%" alignSelf={"flex-end"} >
-                                    <Button borderRadius="10" _pressed={{ bg: 'tema.6' }} _dark={{ bg: "tema.3" }} _light={{ bg: "tema.2" }} onPress={() => setIsOpen3(!Confirm)}>Confirmar
-                                        <AlertDialog isOpen={Confirm} onClose={CloseC}>
-                                            <AlertDialog.Content _dark={{ bg: "tema.2" }} _light={{ bg: "tema.3" }}>
-                                                <AlertDialog.Body _dark={{ bg: "tema.2" }} _light={{ bg: "tema.3" }} alignSelf="center">
-                                                    <TouchableOpacity onPress={CloseS}>
-                                                        <Image size="5" marginLeft="90%" marginBottom="10%" source={require('../assets/XA.png')} _dark={{ color: "tema.3", tintColor: "tema.3" }} _light={{ color: "tema.2", tintColor: "tema.2" }} alt="close" />
-                                                    </TouchableOpacity>
-                                                    <Text _dark={{ color: "tema.3" }} _light={{ color: "tema.2" }} fontSize="md" textAlign="center">¡Información enviada correctamente!</Text>
-                                                    <Button marginTop="10%" alignSelf="center" w="90px" borderRadius="10" _pressed={{ bg: 'tema.6' }} _dark={{ bg: "tema.3" }} _light={{ bg: "tema.2" }} onPress={CloseS}>Aceptar</Button>
-                                                </AlertDialog.Body>
-                                            </AlertDialog.Content>
-                                        </AlertDialog>
-                                    </Button>
-                                    <Button borderRadius="10" _pressed={{ bg: 'tema.6' }} _dark={{ bg: "tema.3" }} _light={{ bg: "tema.2" }} onPress={CloseS}>Cancelar</Button>
-                                </Button.Group>
-                            </AlertDialog.Body>
-                        </AlertDialog.Content>
-                    </AlertDialog>
-                </Button>
-                <Button marginTop="3%" rounded="lg" w="40%" alignSelf="center" _pressed={{ bg: 'tema.6' }} _dark={{ bg: "tema.3" }} _light={{ bg: "tema.2" }}>Cancelar</Button>
+            <Text _dark={{ color: "tema.2" }} _light={{ color: "tema.3" }} alignSelf="flex-end" fontSize="md">Folio: 001</Text>
+
+<View>
+    <Text _dark={{ color: "tema.2" }} _light={{ color: "tema.3" }} fontSize="md" marginTop="3%">Fecha de mantenimiento: 
+        <TouchableOpacity onPress={handleOnPress}>
+            <Text _dark={{ color: "tema.2" }} _light={{ color: "tema.3" }} fontSize="md"> {text}</Text>
+        </TouchableOpacity>
+    </Text>
+            
+    <Modal transparent={true} visible={openM}>
+        <Box height="70%" width="80%" alignSelf="center" marginTop="20%">
+            <DatePicker mode="calendar" minimumDate={startDate} selected={date} onDateChange={handleChange}/>
+            <Button _pressed={{ bg: 'tema.6' }} _dark={{ bg: "tema.2" }} _light={{ bg: "tema.3" }}>
+                <TouchableOpacity onPress={handleOnPress}>
+                    <Text _dark={{ color: "tema.3" }} _light={{ color: "tema.2" }} fontSize="md" alignSelf="flex-end">Cerrar</Text>
+                </TouchableOpacity>
+            </Button>
+        </Box>
+    </Modal>
+</View>
+
+<Text _dark={{ color: "tema.2" }} _light={{ color: "tema.3" }} fontSize="md" marginTop="3%">Trabajo realizado:</Text>
+<Input _dark={{ bg: "tema.2", color: "tema.3", placeholderTextColor: "tema.3" }} _light={{ bg: "tema.3", color: "tema.2", placeholderTextColor: "tema.2" }} borderColor="tema.1" fontSize="md" height="15%" />
+
+<Text _dark={{ color: "tema.2" }} _light={{ color: "tema.3" }} fontSize="md" marginTop="3%">Añadir Evidencia:</Text>
+<HStack space="4" alignSelf="center" marginTop="3%">
+    <Button h="20" w="20" borderWidth={0.5} _pressed={{ bg: 'tema.6' }} _dark={{ bg: "tema.3" }} _light={{ bg: "tema.2" }} onPress={openGallery}>
+        <Image size="10" source={require('../assets/Image.png')} _dark={{ color: "tema.2", tintColor: "tema.2" }} _light={{ color: "tema.3", tintColor: "tema.3" }} alt="Add image" />
+    </Button>
+    <Image alignSelf="center" source={{uri: galleryPhoto}} alt="Image gallery"/>
+
+    <Button h="20" w="20" borderWidth={0.5} _pressed={{ bg: 'tema.6' }} _dark={{ bg: "tema.3" }} _light={{ bg: "tema.2" }} onPress={openGallery}>
+        <Image size="10" source={require('../assets/Image.png')} _dark={{ color: "tema.2", tintColor: "tema.2" }} _light={{ color: "tema.3", tintColor: "tema.3" }} alt="Add image" />
+    </Button>
+    <Image alignSelf="center" source={{uri: galleryPhoto}} alt="Image gallery"/>
+
+    <Button h="20" w="20" borderWidth={0.5} _pressed={{ bg: 'tema.6' }} _dark={{ bg: "tema.3" }} _light={{ bg: "tema.2" }} onPress={openGallery}>
+        <Image size="10" source={require('../assets/Image.png')} _dark={{ color: "tema.2", tintColor: "tema.2" }} _light={{ color: "tema.3", tintColor: "tema.3" }} alt="Add image" />
+    </Button>
+    <Image alignSelf="center" source={{uri: galleryPhoto}} alt="Image gallery"/>
+</HStack>
+
+<Button onPress={() => setIsOpen2(!Send)} marginTop="10%" rounded="lg" w="40%" alignSelf="center" _pressed={{ bg: 'tema.6' }} _dark={{ bg: "tema.2" }} _light={{ bg: "tema.3" }}>
+    <Text _dark={{color: "tema.3"}} _light={{color: "tema.2"}} fontSize="md" textAlign="center">Enviar</Text>
+    <AlertDialog isOpen={Send} onClose={CloseS}>
+        <AlertDialog.Content _dark={{bg: "tema.2"}} _light={{bg: "tema.3"}}>
+            <AlertDialog.Body _dark={{bg: "tema.2"}} _light={{bg: "tema.3"}} alignSelf="center">
+                <TouchableOpacity onPress={CloseS}>
+                    <Image size="5" marginLeft="90%" marginBottom="10%" source={require('../assets/XA.png')} _dark={{ color: "tema.3", tintColor: "tema.3" }} _light={{ color: "tema.2", tintColor: "tema.2" }} alt="close" />
+                </TouchableOpacity> 
+                <Text _dark={{color: "tema.3"}} _light={{color: "tema.2"}} fontSize="md" textAlign="center">¿Esta seguro de enviar la Información?</Text>
+                <Button.Group marginTop="10%" alignSelf={"flex-end"} >
+                    <Button borderRadius="10" _pressed={{ bg: 'tema.6' }} _dark={{ bg: "tema.3" }} _light={{ bg: "tema.2" }} onPress={() => setIsOpen3(!Confirm)}>Confirmar
+                    <AlertDialog isOpen={Confirm} onClose={CloseC}>
+                            <AlertDialog.Content _dark={{ color: "tema.2" }} _light={{ color: "tema.3" }}>
+                                <AlertDialog.Body _dark={{bg: "tema.2"}} _light={{bg: "tema.3"}} alignSelf="center">
+                                    <TouchableOpacity onPress={CloseS}>
+                                        <Image size="5" marginLeft="90%" marginBottom="10%" source={require('../assets/XA.png')} _dark={{ color: "tema.2", tintColor: "tema.2" }} _light={{ color: "tema.3", tintColor: "tema.3" }} alt="close" />
+                                    </TouchableOpacity> 
+                                    <Text _dark={{color: "tema.3"}} _light={{color: "tema.2"}} fontSize="md" textAlign="center">¡Información enviada correctamente!</Text> 
+                                    <Button marginTop="10%" alignSelf="center" w="90px" borderRadius="10" _pressed={{ bg: 'tema.6' }} _dark={{ bg: "tema.3" }} _light={{ bg: "tema.2" }} onPress={CloseS}>Aceptar</Button>
+                                </AlertDialog.Body>
+                            </AlertDialog.Content>
+                        </AlertDialog>
+                    </Button>
+                    <Button borderRadius="10" _pressed={{ bg: 'tema.6' }} _dark={{ bg: "tema.3" }} _light={{ bg: "tema.2" }} onPress={CloseS}>Cancelar</Button>
+                </Button.Group>
+            </AlertDialog.Body>
+        </AlertDialog.Content>
+    </AlertDialog>
+</Button>
+<Button marginTop="3%" rounded="lg" w="40%" alignSelf="center" _pressed={{ bg: 'tema.6' }} _dark={{ bg: "tema.2" }} _light={{ bg: "tema.3" }}>
+    <Text _dark={{color: "tema.3"}} _light={{color: "tema.2"}} fontSize="md" textAlign="center">Cancelar</Text>
+</Button>
             </Box>
 
             <HStack height="10%" alignItems="center" alignSelf="center" space="10">
--- components/ViewWorkOrderAprovedRealeased.js
+++ components/ViewWorkOrderAprovedRealeased.js
@@ -1,31 +1,155 @@
-import React from "react";
-import { VStack, Text, Heading, Image, Box, HStack, AlertDialog, Button, TextArea, Divider } from "native-base";
-import { TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { VStack, Text, Image, Box, HStack, AlertDialog, Button, Input } from "native-base";
+import DatePicker, { getFormatedDate } from "react-native-modern-datepicker";
+import { View, Modal, TouchableOpacity } from "react-native";
+import { launchImageLibrary } from "react-native-image-picker";
 
-function VieWorOrdApr({navigation}){
+function VieWorOrdApr({ navigation }) {
+    //Estados para abrir y cerrar alerts
     const [Exit, setIsOpen1] = React.useState(false);
     const CloseE = () => setIsOpen1(false);
 
-    return(
+    //Estados para seleccionar fecha
+    const todayS = new Date();
+    const startDateS = getFormatedDate(todayS.setDate(todayS.getDate() + 1), 'DD/MM/YYYY')
+
+    const [openS, setOpenS] = useState(false); //abrir y cerrar modal
+    const [dateS, setDateS] = useState('Seleccionar'); //variable de fecha
+    const [textS, setTextS] = useState('Seleccionar');
+
+    function handleOnPress() {
+        setOpenS(!openS);
+    }
+
+    function handleChange(propDate) {
+        setDateS(propDate)
+        setTextS(dateS)
+    }
+
+    const todayM = new Date();
+    const startDateM = getFormatedDate(todayM.setDate(todayM.getDate() + 1), 'DD/MM/YYYY')
+
+    const [openM, setOpenM] = useState(false); //abrir y cerrar modal
+    const [dateM, setDateM] = useState('Seleccionar'); //variable de fecha
+    const [textM, setTextM] = useState('Seleccionar');
+
+    function handleOnPressM() {
+        setOpenM(!openM);
+    }
+
+    function handleChangeM(propDate) {
+        setDateM(propDate)
+        setTextM(dateM)
+    }
+
+    //Estados para cargar imagen
+    const [galleryPhoto, setGalleryPhoto] = useState();
+
+    let options = {
+        saveToPhotos: true,
+        mediaType: 'photo',
+    }
+
+    const openGallery = async () => {
+        const result = await launchImageLibrary(options);
+        setGalleryPhoto(result.assets[0].uri)
+    }
+
+    return (
         <VStack height="100%" width="100%" space={4} _dark={{ bg: "tema.3" }} _light={{ bg: "tema.2" }}>
-           
+
             <Heading _dark={{ color: "tema.2" }} _light={{ color: "tema.3" }} textAlign="center" height="15%" alignSelf="center" fontSize="3xl" paddingTop="10%">Órden de trabajo de Mantenimiento</Heading>
 
             <Box height="70%" w="90%" alignSelf="center">
-                <Text _dark={{ color: "tema.2" }} _light={{ color: "tema.3" }} fontSize="md" marginTop="3%">Fecha de solicitud: 28/02/23</Text>
-                <Text _dark={{ color: "tema.2" }} _light={{ color: "tema.3" }} fontSize="md" marginTop="3%">Área solicitante: Mantenimiento de Equipo.</Text>
-                <Text _dark={{ color: "tema.2" }} _light={{ color: "tema.3" }} fontSize="md" marginTop="3%">Nombre solicitante: M.C. Alfonso Recio Hernández.</Text>
-                <Text _dark={{ color: "tema.2" }} _light={{ color: "tema.3" }} fontSize="md" marginTop="3%">Descripción:</Text>
-                <TextArea h="16"></TextArea>
-                <Text _dark={{ color: "tema.2" }} _light={{ color: "tema.3" }} fontSize="md" marginTop="3%">Evidencia:</Text>
-                <Text _dark={{ color: "tema.2" }} _light={{ color: "tema.3" }} fontSize="md" marginTop="3%">(Imagen1)  (Imagen2)  (Imagen3)</Text>
-                <Text _dark={{ color: "tema.2" }} _light={{ color: "tema.3" }} fontSize="md" marginTop="3%">Fecha de mantenimiento: 01/03/23</Text>
-                <Text _dark={{ color: "tema.2" }} _light={{ color: "tema.3" }} fontSize="md" marginTop="3%">Trabajo realizado:</Text>
-                <TextArea h="16"  _dark={{color: "tema.3"}} _light={{color: "tema.2"}}></TextArea>
-                <Text _dark={{ color: "tema.2" }} _light={{ color: "tema.3" }} fontSize="md" marginTop="3%">Evidencia de trabajo:</Text>
-                <Text _dark={{ color: "tema.2" }} _light={{ color: "tema.3" }} fontSize="md" marginTop="3%">(Imagen1)  (Imagen2)  (Imagen3)</Text>
-                {/* El status se debe cambiar según el que tenga en la BD */}
-                <Text _dark={{ color: "tema.2" }} _light={{ color: "tema.3" }} fontSize="md" marginTop="3%" textAlign="center">Estatus: <Text color="orange.400">LIBERADA</Text></Text>
+
+                <View>
+                    <Text _dark={{ color: "tema.2" }} _light={{ color: "tema.3" }} fontSize="md" marginTop="3%" fontWeight="bold">Fecha de solicitud:
+                        <TouchableOpacity onPress={handleOnPress}>
+                            <Text _dark={{ color: "tema.2" }} _light={{ color: "tema.3" }} fontSize="md"> {textS}</Text>
+                        </TouchableOpacity>
+                    </Text>
+
+                    <Modal transparent={true} visible={openS}>
+                        <Box height="70%" width="80%" alignSelf="center" marginTop="20%">
+                            <DatePicker mode="calendar" minimumDate={startDateS} selected={dateS} onDateChange={handleChange} />
+                            <Button _pressed={{ bg: 'tema.6' }} _dark={{ bg: "tema.2" }} _light={{ bg: "tema.3" }}>
+                                <TouchableOpacity onPress={handleOnPress}>
+                                    <Text _dark={{ color: "tema.3" }} _light={{ color: "tema.2" }} fontSize="md" alignSelf="flex-end">Cerrar</Text>
+                                </TouchableOpacity>
+                            </Button>
+                        </Box>
+                    </Modal>
+                </View>
+
+                <Text _dark={{ color: "tema.2" }} _light={{ color: "tema.3" }} fontSize="md" marginTop="3%" fontWeight="bold">Área solicitante: </Text>
+                <Input _dark={{ bg: "tema.2", color: "tema.3", placeholderTextColor: "tema.3" }} _light={{ bg: "tema.3", color: "tema.2", placeholderTextColor: "tema.2" }} borderColor="tema.1" fontSize="md" height="6%" />
+
+                <Text _dark={{ color: "tema.2" }} _light={{ color: "tema.3" }} fontSize="md" marginTop="3%" fontWeight="bold">Nombre solicitante: </Text>
+                <Input _dark={{ bg: "tema.2", color: "tema.3", placeholderTextColor: "tema.3" }} _light={{ bg: "tema.3", color: "tema.2", placeholderTextColor: "tema.2" }} borderColor="tema.1" fontSize="md" height="6%" />
+
+                <Text _dark={{ color: "tema.2" }} _light={{ color: "tema.3" }} fontSize="md" marginTop="3%" fontWeight="bold">Descripción:</Text>
+                <Input _dark={{ bg: "tema.2", color: "tema.3", placeholderTextColor: "tema.3" }} _light={{ bg: "tema.3", color: "tema.2", placeholderTextColor: "tema.2" }} borderColor="tema.1" fontSize="md" height="8%" />
+
+                <Text _dark={{ color: "tema.2" }} _light={{ color: "tema.3" }} fontSize="md" marginTop="3%" fontWeight="bold">Evidencia:</Text>
+                <HStack space="4" alignSelf="center" marginTop="3%">
+                    <Button h="20" w="20" borderWidth={0.5} _pressed={{ bg: 'tema.6' }} _dark={{ bg: "tema.3" }} _light={{ bg: "tema.2" }} onPress={openGallery}>
+                        <Image size="10" source={require('../assets/Image.png')} _dark={{ color: "tema.2", tintColor: "tema.2" }} _light={{ color: "tema.3", tintColor: "tema.3" }} alt="Add image" />
+                    </Button>
+                    <Image alignSelf="center" source={{ uri: galleryPhoto }} alt="Image gallery" />
+
+                    <Button h="20" w="20" borderWidth={0.5} _pressed={{ bg: 'tema.6' }} _dark={{ bg: "tema.3" }} _light={{ bg: "tema.2" }} onPress={openGallery}>
+                        <Image size="10" source={require('../assets/Image.png')} _dark={{ color: "tema.2", tintColor: "tema.2" }} _light={{ color: "tema.3", tintColor: "tema.3" }} alt="Add image" />
+                    </Button>
+                    <Image alignSelf="center" source={{ uri: galleryPhoto }} alt="Image gallery" />
+
+                    <Button h="20" w="20" borderWidth={0.5} _pressed={{ bg: 'tema.6' }} _dark={{ bg: "tema.3" }} _light={{ bg: "tema.2" }} onPress={openGallery}>
+                        <Image size="10" source={require('../assets/Image.png')} _dark={{ color: "tema.2", tintColor: "tema.2" }} _light={{ color: "tema.3", tintColor: "tema.3" }} alt="Add image" />
+                    </Button>
+                    <Image alignSelf="center" source={{ uri: galleryPhoto }} alt="Image gallery" />
+                </HStack>
+
+                <View>
+                    <Text _dark={{ color: "tema.2" }} _light={{ color: "tema.3" }} fontSize="md" marginTop="3%" fontWeight="bold">Fecha de mantenimiento:
+                        <TouchableOpacity onPress={handleOnPressM}>
+                            <Text _dark={{ color: "tema.2" }} _light={{ color: "tema.3" }} fontSize="md"> {textM}</Text>
+                        </TouchableOpacity>
+                    </Text>
+
+                    <Modal transparent={true} visible={openM}>
+                        <Box height="70%" width="80%" alignSelf="center" marginTop="20%">
+                            <DatePicker mode="calendar" minimumDate={startDateM} selected={dateM} onDateChange={handleChangeM} />
+                            <Button _pressed={{ bg: 'tema.6' }} _dark={{ bg: "tema.2" }} _light={{ bg: "tema.3" }}>
+                                <TouchableOpacity onPress={handleOnPressM}>
+                                    <Text _dark={{ color: "tema.3" }} _light={{ color: "tema.2" }} fontSize="md" alignSelf="flex-end">Cerrar</Text>
+                                </TouchableOpacity>
+                            </Button>
+                        </Box>
+                    </Modal>
+                </View>
+
+                <Text _dark={{ color: "tema.2" }} _light={{ color: "tema.3" }} fontSize="md" marginTop="3%" fontWeight="bold">Trabajo realizado:</Text>
+                <Input _dark={{ bg: "tema.2", color: "tema.3", placeholderTextColor: "tema.3" }} _light={{ bg: "tema.3", color: "tema.2", placeholderTextColor: "tema.2" }} borderColor="tema.1" fontSize="md" height="8%" />
+
+                <Text _dark={{ color: "tema.2" }} _light={{ color: "tema.3" }} fontSize="md" marginTop="3%" fontWeight="bold">Evidencia de trabajo:</Text>
+                <HStack space="4" alignSelf="center" marginTop="3%">
+                    <Button h="20" w="20" borderWidth={0.5} _pressed={{ bg: 'tema.6' }} _dark={{ bg: "tema.3" }} _light={{ bg: "tema.2" }} onPress={openGallery}>
+                        <Image size="10" source={require('../assets/Image.png')} _dark={{ color: "tema.2", tintColor: "tema.2" }} _light={{ color: "tema.3", tintColor: "tema.3" }} alt="Add image" />
+                    </Button>
+                    <Image alignSelf="center" source={{ uri: galleryPhoto }} alt="Image gallery" />
+
+                    <Button h="20" w="20" borderWidth={0.5} _pressed={{ bg: 'tema.6' }} _dark={{ bg: "tema.3" }} _light={{ bg: "tema.2" }} onPress={openGallery}>
+                        <Image size="10" source={require('../assets/Image.png')} _dark={{ color: "tema.2", tintColor: "tema.2" }} _light={{ color: "tema.3", tintColor: "tema.3" }} alt="Add image" />
+                    </Button>
+                    <Image alignSelf="center" source={{ uri: galleryPhoto }} alt="Image gallery" />
+
+                    <Button h="20" w="20" borderWidth={0.5} _pressed={{ bg: 'tema.6' }} _dark={{ bg: "tema.3" }} _light={{ bg: "tema.2" }} onPress={openGallery}>
+                        <Image size="10" source={require('../assets/Image.png')} _dark={{ color: "tema.2", tintColor: "tema.2" }} _light={{ color: "tema.3", tintColor: "tema.3" }} alt="Add image" />
+                    </Button>
+                    <Image alignSelf="center" source={{ uri: galleryPhoto }} alt="Image gallery" />
+                </HStack>
+
+                <Text _dark={{ color: "tema.2" }} _light={{ color: "tema.3" }} fontSize="md" marginTop="3%" textAlign="center" fontWeight="bold">Estatus: <Text color="orange.400">LIBERADA</Text></Text>
+
             </Box>
 
             <HStack height="10%" alignItems="center" alignSelf="center" space="10">
@@ -41,12 +165,12 @@ function VieWorOrdApr({navigation}){
                 <TouchableOpacity onPress={() => setIsOpen1(!Exit)}>
                     <Image size="10" source={require('../assets/CS2B.png')} _dark={{ color: "tema.2", tintColor: "tema.2" }} _light={{ color: "tema.3", tintColor: "tema.3" }} alt="exit" />
                     <AlertDialog isOpen={Exit} onClose={CloseE}>
-                        <AlertDialog.Content _dark={{bg: "tema.2"}} _light={{bg: "tema.3"}}>
-                            <AlertDialog.Body _dark={{bg: "tema.2"}} _light={{bg: "tema.3"}} alignSelf="center">
+                        <AlertDialog.Content _dark={{ bg: "tema.2" }} _light={{ bg: "tema.3" }}>
+                            <AlertDialog.Body _dark={{ bg: "tema.2" }} _light={{ bg: "tema.3" }} alignSelf="center">
                                 <TouchableOpacity onPress={CloseE}>
                                     <Image size="5" marginLeft="90%" marginBottom="10%" source={require('../assets/XA.png')} _dark={{ color: "tema.3", tintColor: "tema.3" }} _light={{ color: "tema.2", tintColor: "tema.2" }} alt="close" />
                                 </TouchableOpacity>
-                                <Text _dark={{color: "tema.3"}} _light={{color: "tema.2"}} fontSize="md" textAlign="center">¿Esta seguro de cerrar sesión?</Text>
+                                <Text _dark={{ color: "tema.3" }} _light={{ color: "tema.2" }} fontSize="md" textAlign="center">¿Esta seguro de cerrar sesión?</Text>
                                 <Button.Group marginTop="10%" alignSelf={"flex-end"} >
                                     <Button borderRadius="10" _pressed={{ bg: 'tema.6' }} _dark={{ bg: "tema.3" }} _light={{ bg: "tema.2" }} onPress={() => navigation.navigate("login")}>
                                         <Text _dark={{ color: "tema.2" }} _light={{ color: "tema.3" }}>Confirmar</Text>
