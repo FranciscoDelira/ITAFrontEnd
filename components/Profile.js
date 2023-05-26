c6ffc662861a5609b9d5596a52c8b3de6e4a4ad1
Mariana0416 <sanchezmariana2307@gmail.com>
2023-05-25 20:37:57 -0600

    update

--- components/Profile.js
+++ components/Profile.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Box, Text, VStack, Heading, Image, HStack, Avatar, AlertDialog, Button} from "native-base";
+import { Text, VStack, Heading, Image, HStack, Avatar, AlertDialog, Button} from "native-base";
 import { TouchableOpacity } from "react-native";
 
 
@@ -21,7 +21,7 @@ function Profile ({ navigation, route }) {
         // Función para obtener los datos del personaldata_id
         const getPersonalData = async () => {
           try {
-            const response = await fetch(`http://192.168.8.103/ITABackEnd/public/api/personalData_show/${personaldata_id}`);
+            const response = await fetch(`http://10.0.9.8/ITABackEnd/public/api/personalData_show/${personaldata_id}`);
             const data = await response.json();
             setPersonalData(data); // Actualiza el estado con los datos obtenidos
           } catch (error) {
@@ -36,7 +36,7 @@ function Profile ({ navigation, route }) {
         //Función para obtener los datos del usuario
         const getUser = async () => {
             try {
-                const response = await fetch(`http://192.168.8.103/ITABackEnd/public/api/user_show/${id}`);
+                const response = await fetch(`http://10.0.9.8/ITABackEnd/public/api/user_show/${id}`);
                 const data = await response.json();
                 setUser(data);//Actualiza el estado con los datos obtenidos
             }catch (error) {
@@ -44,7 +44,6 @@ function Profile ({ navigation, route }) {
             }
         };
         getUser(); //Llama la función para obtener los datos
-
       }, [id]);
 
       const validateRole = async () => {
@@ -53,33 +52,28 @@ function Profile ({ navigation, route }) {
             navigation.navigate("menmaireq", {personaldata_id: personaldata_id, id: id});
           } else if (user?.role === 'Mantenimiento') {
             console.log('navigation work order');
-            navigation.navigate("menworord", {personaldata_id: personaldata_id, id: id});
+            navigation.navigate("menworord", {personaldata_id: personaldata_id, id: id}, {personaldata_id: personaldata_id, id:id});
           }
       }
 
     return (
-        <VStack height="100%" width="100%" space={4} _dark={{bg:"tema.3"}} _light={{bg:"tema.2"}}>
+        <VStack height="100%" width="100%" _dark={{bg:"tema.3"}} _light={{bg:"tema.2"}}>
            
-            <Heading height="20%" alignSelf="center" _dark={{color:"tema.2"}} _light={{color:"tema.3"}} fontSize="4xl" marginTop="10%">Perfil de Usuario</Heading>
+            <Heading height="20%" alignSelf="center" _dark={{color:"tema.2"}} _light={{color:"tema.3"}} fontSize="4xl" paddingTop="10%">Perfil de Usuario</Heading>
            
-            <Avatar height="20%" width="40%" alignSelf="center" source={{
+            <Avatar height="20%" width="40%" alignSelf="center" marginTop="-10%" source={{
                 uri: "https://www.w3schools.com/css/img_lights.jpg" }}>
             </Avatar>
 
-            <Box height="40%" alignSelf="center" p="2">
-                <Text _dark={{color:"tema.2"}} _light={{color:"tema.3"}} fontSize="3xl" >Nombre: </Text>
-                <Text _dark={{color:"tema.2"}} _light={{color:"tema.3"}} p="2">{personalData?.name}</Text>
-                <Text _dark={{color:"tema.2"}} _light={{color:"tema.3"}} fontSize="3xl" >Apellidos: </Text>
-                <Text _dark={{color:"tema.2"}} _light={{color:"tema.3"}} p="2">{personalData?.lastname}</Text>
-                <Text _dark={{color:"tema.2"}} _light={{color:"tema.3"}} fontSize="3xl" >Correo: </Text>
-                <Text _dark={{color:"tema.2"}} _light={{color:"tema.3"}} p="2">{user?.email}</Text>
-                <Text _dark={{color:"tema.2"}} _light={{color:"tema.3"}} fontSize="3xl" >Area: </Text>
-                <Text _dark={{color:"tema.2"}} _light={{color:"tema.3"}} p="2">{personalData?.area}</Text>
-                <Text _dark={{color:"tema.2"}} _light={{color:"tema.3"}} fontSize="3xl" >Plantel: </Text>
-                <Text _dark={{color:"tema.2"}} _light={{color:"tema.3"}} p="2">{personalData?.plantel}</Text>
-            </Box>
+            <VStack height="40%" alignSelf="center" space={5} marginTop="10%">
+                <Text _dark={{color:"tema.2"}} _light={{color:"tema.3"}} fontSize="2xl" >Nombre: {personalData?.name}</Text>
+                <Text _dark={{color:"tema.2"}} _light={{color:"tema.3"}} fontSize="2xl" >Apellidos: {personalData?.lastname}</Text>
+                <Text _dark={{color:"tema.2"}} _light={{color:"tema.3"}} fontSize="2xl" >Correo: {user?.email}</Text>
+                <Text _dark={{color:"tema.2"}} _light={{color:"tema.3"}} fontSize="2xl" >Area: {personalData?.area}</Text>
+                <Text _dark={{color:"tema.2"}} _light={{color:"tema.3"}} fontSize="2xl" >Plantel: {personalData?.plantel}</Text>
+            </VStack>
 
-            <HStack height="10%" alignItems="center" alignSelf="center" space="1/6">    
+            <HStack height="10%" alignItems="center" alignSelf="center" space="1/6" marginTop="20%">    
               <TouchableOpacity onPress={validateRole}>
                     <Image size="10" source={require('../assets/C1B.png')} _dark={{ color: "tema.2", tintColor: "tema.2" }} _light={{ color: "tema.3", tintColor: "tema.3" }} alt="home" />
                 </TouchableOpacity>
