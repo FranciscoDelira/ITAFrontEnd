74032938d2bc79a29a77f51df738e72ed148c614
Mariana0416 <sanchezmariana2307@gmail.com>
2023-06-06 21:36:54 -0600

    functionalities workOrder Mariana

--- components/MenuWorkOrderReleased.js
+++ components/MenuWorkOrderReleased.js
@@ -1,18 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Box, Button, VStack, Heading, Image, HStack, AlertDialog, Text } from "native-base";
 import { TouchableOpacity } from "react-native";
+import axios from "axios";
 
 function MenWorOrdRel({ navigation, route }) {
 
     /* recibe los parametros del id del user y del personal data */
     const { personaldata_id } = route.params;
     console.log('Menu Work Order Released')
-    console.log('PersonalData ID:', personaldata_id);
+   // console.log('PersonalData ID:', personaldata_id);
     const { id } = route.params;
-    console.log('User ID:', id);
-
+   // console.log('User ID:', id);
     const [Exit, setIsOpen1] = React.useState(false);
     const CloseE = () => setIsOpen1(false);
+
+    const [workOrderR, setWorkOrderR] = useState([]);
+
+    useEffect(() => {
+        getWorkOrderReleased()
+    });
+
+    const getWorkOrderReleased = async () => {
+        try {
+            const response = await axios.get(
+                `http://192.168.100.93/ITABackEnd/public/api/workorder_released/${personaldata_id}`,
+                {
+                    headers: {
+                        'Content-Type': 'multipart/form-data',
+                        "Access-Control-Allow-Origin": "*"
+                    }
+                }
+            );
+
+            setWorkOrderR(response.data);
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
     return (
         <VStack height="100%" width="100%" space={4} _dark={{ bg: "tema.3" }} _light={{ bg: "tema.2" }}>
@@ -22,28 +47,28 @@ function MenWorOrdRel({ navigation, route }) {
             <Heading height="15%" textAlign={"center"} alignSelf="center" _dark={{ color: "tema.2" }} _light={{ color: "tema.3" }} fontSize="4xl">Ordenes de trabajo liberadas</Heading>
 
             <Box height="55%" w="90%" alignSelf="center" >
-               
-                <Box _dark={{ bg: "tema.2", color: "tema.3" }} _light={{ bg: "tema.3", color: "tema.2" }} _pressed={{ bg: 'tema.6' }} borderRadius="xl" height={"32%"} marginBottom="5%" padding={"3%"}>
-                    <Text bold fontSize="xs" _dark={{ color: "tema.3" }} _light={{ color: "tema.2" }}>Folio: </Text>
-                    <Text bold fontSize="xs" _dark={{ color: "tema.3" }} _light={{ color: "tema.2" }}>Area solicitante: </Text>
-                    <Text bold fontSize="xs" _dark={{ color: "tema.3" }} _light={{ color: "tema.2" }}>Fecha de solicitud: </Text>
-                    <HStack marginTop={"8%"} alignItems="center">
-                        <Text bold fontSize="sm" _dark={{ color: "tema.9" }} _light={{ color: "tema.9" }}>LIBERADA</Text>
-                        <Button size="9" borderRadius={25} marginLeft={"58%"} variant="unstyled" _pressed={{ bg: 'tema.6' }} onPress={() => navigation.navigate("vieworordapr", { personaldata_id: personaldata_id, id: id })}>
-                            <Image size="7" source={require('../assets/PL1N.png')} _dark={{ color: "tema.3", tintColor: "tema.3" }} _light={{ color: "tema.2", tintColor: "tema.2" }} alt="open" />
-                        </Button>
-                    </HStack>
-                </Box>
-           
+                {workOrderR.map((WOReleased) => (
+                    <Box _dark={{ bg: "tema.2", color: "tema.3" }} _light={{ bg: "tema.3", color: "tema.2" }} _pressed={{ bg: 'tema.6' }} borderRadius="xl" height={"32%"} marginBottom="5%" padding={"3%"} key={WOReleased.id}>
+                        <Text bold fontSize="xs" _dark={{ color: "tema.3" }} _light={{ color: "tema.2" }}>Folio: {WOReleased.id}</Text>
+                        <Text bold fontSize="xs" _dark={{ color: "tema.3" }} _light={{ color: "tema.2" }}>Area solicitante: {WOReleased.department}</Text>
+                        <Text bold fontSize="xs" _dark={{ color: "tema.3" }} _light={{ color: "tema.2" }}>Fecha de solicitud: {WOReleased.requestDate}</Text>
+                        <HStack marginTop={"8%"} alignItems="center">
+                            <Text bold fontSize="sm" _dark={{ color: "tema.10" }} _light={{ color: "tema.10" }}>{WOReleased.status.toUpperCase()}</Text>
+                            <Button size="9" borderRadius={25} marginLeft={"58%"} variant="unstyled" _pressed={{ bg: 'tema.6' }} onPress={() => navigation.navigate("vieworordapr", { personaldata_id: personaldata_id, id: id })}>
+                                <Image size="7" source={require('../assets/PL1N.png')} _dark={{ color: "tema.3", tintColor: "tema.3" }} _light={{ color: "tema.2", tintColor: "tema.2" }} alt="open" />
+                            </Button>
+                        </HStack>
+                    </Box>
+                ))}
             </Box>
 
             <HStack height="10%" alignItems="center" alignSelf="center" space="10">
 
-                 <TouchableOpacity onPress={() => navigation.navigate("menworord",{ personaldata_id: personaldata_id, id: id })}>
+                <TouchableOpacity onPress={() => navigation.navigate("menworord", { personaldata_id: personaldata_id, id: id })}>
                     <Image size="10" source={require('../assets/C1B.png')} alt="maintenance" />
                 </TouchableOpacity>
-                
-                <TouchableOpacity onPress={() => navigation.navigate("settings",{ personaldata_id: personaldata_id, id: id })}>
+
+                <TouchableOpacity onPress={() => navigation.navigate("settings", { personaldata_id: personaldata_id, id: id })}>
                     <Image size="10" source={require('../assets/SE2N.png')} _dark={{ color: "tema.2", tintColor: "tema.2" }} _light={{ color: "tema.3", tintColor: "tema.3" }} alt="password" />
                 </TouchableOpacity>
 
